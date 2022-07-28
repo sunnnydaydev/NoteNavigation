75046f8033b7dd440e0cea1b879780525030dcfc
SunnyDay <1246771571@qq.com>
2022-07-28 21:38:25 +0800

    嵌套导航图ing +1

--- app/src/main/java/com/example/notenavigation/NestActivity.kt
+++ app/src/main/java/com/example/notenavigation/NestActivity.kt
@@ -2,10 +2,21 @@ package com.example.notenavigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import com.example.notenavigation.nest.HomeFragmentDirections
 
 class NestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nest)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment
+
+        val navController = navHostFragment.navController
+
+        // 跳转到第二个导航图的起始目的地
+        navController.navigate(HomeFragmentDirections.actionHomeFragmentToSendMoneyGraph())
+
+
     }
 }
