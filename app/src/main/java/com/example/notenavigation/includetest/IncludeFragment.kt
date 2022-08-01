0c17f2fddb9f40fe13115b5af44be5601c18dcb0
SunnyDay <1246771571@qq.com>
2022-08-01 21:13:45 +0800

    add some readmes

--- app/src/main/java/com/example/notenavigation/includetest/IncludeFragment.kt
+++ app/src/main/java/com/example/notenavigation/includetest/IncludeFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavDestination
+import androidx.navigation.fragment.findNavController
+import com.example.notenavigation.NavNestGraphDirections
 import com.example.notenavigation.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -34,7 +37,9 @@ class IncludeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
+        findNavController().navigate(NavNestGraphDirections.actionGlobalHomeFragment())
+
         return inflater.inflate(R.layout.fragment_include, container, false)
     }
 
